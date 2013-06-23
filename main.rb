da696e0354261e183f779b586e6237ec12c3f1c1
Elzair <elzairthesorcerer@gmail.com>
2013-06-22 22:06:46 -0400

    split parser functionality into its own file

--- main.rb
+++ main.rb
@@ -1,15 +1,6 @@
 #!/usr/bin/env ruby
 
-class Token
-  # Create a new Token
-  def initialize(value = "")
-    @value = value
-  end
-
-  def to_s
-    return @value
-  end
-end
+require './parser.rb'
 
 class InputHandler
   # Create new object
---
+++ parser.rb
@@ -0,0 +1,12 @@
+
+class Token
+  # Create a new Token
+  def initialize(value = "")
+    @value = value
+  end
+
+  def to_s
+    return @value
+  end
+end
+
